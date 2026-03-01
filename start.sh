74626eb6f47818520206d452c64ca6a3bb9e0da4
MFOFANA <mf008355@gmail.com>
2026-03-01 18:05:56 +0000

    Create start.sh
    "Auto-Restart" script now so the bot stays online all night while sleeping

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# --- CLOUD GAMING-223 | AUTO-RESTART SYSTEM ---
+echo "🛰️ Starting Digital Engine V2.7..."
+
+while true
+do
+    # Run the bot
+    node index.js
+
+    # If the bot crashes or stops, wait 5 seconds and restart
+    echo "⚠️ Engine stopped or crashed! Restarting in 5 seconds..."
+    sleep 5
+    echo "🔄 Rebooting System..."
+done
